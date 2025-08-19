fffac488d1b40cdb758ded91670c3526ab846dce
haifishbro209 <kaloyannikolov016@gmail.com>
2025-08-19 11:59:25 +0200

    fixed disappearing

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const particleArray = [];
 
-window.addEventListener('resize',function(){
+window.addEventListener('resize', function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 });
@@ -14,46 +14,46 @@ const mouse = {
     y: undefined,
 }
 
-canvas.addEventListener('click', function(event){
+canvas.addEventListener('click', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
-   init(mouse.x,mouse.y);
+    init(mouse.x, mouse.y);
 
 });
-    
-canvas.addEventListener('mousemove',function(event){
+
+canvas.addEventListener('mousemove', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
     //init(mouse.x,mouse.y);
 
 });
 
-class Particle{
-    constructor(x,y){
+class Particle {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
         this.size = Math.random() * 5 + 1;    //1 bis 6
-        this.speedX = Math.random() * 2 -1; //+1,5 bis -1.5
-        this.speedY = Math.random() * 2 -1;
+        this.speedX = Math.random() * 2 - 1; //+1,5 bis -1.5
+        this.speedY = Math.random() * 2 - 1;
         this.speedSqr = this.speedX * this.speedX + this.speedY * this.speedY;
         this.dxCenter = 0;
         this.dyCenter = 0;
         this.distanceToCenter = 0;
-        let hue = Math.round(Math.random()*360)
-        this.color = 'hsl('+ hue + ', 100%, 50%)';
-        this.size = Math.round(Math.random()*20);
-        
+        let hue = Math.round(Math.random() * 360)
+        this.color = 'hsl(' + hue + ', 100%, 50%)';
+        this.size = Math.round(Math.random() * 20);
+
     }
-    
-    update(){
+
+    update() {
         this.x += this.speedX;
         this.y += this.speedY;
         this.dxCenter = Math.abs(this.x - canvas.width / 2);
         this.dyCenter = Math.abs(this.y - canvas.height / 2);
-        this.distanceToCenter = Math.sqrt(this.dxCenter * this.dxCenter + this.dyCenter * this.dyCenter); 
+        this.distanceToCenter = Math.sqrt(this.dxCenter * this.dxCenter + this.dyCenter * this.dyCenter);
     }
-    
-    draw(){
+
+    draw() {
         ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
@@ -62,23 +62,26 @@ class Particle{
 }
 
 
-function init(x,y){
-   
-    for(let i = 0; i < 1; i++){
-        particleArray.push(new Particle(x,y));
+function init(x, y) {
+
+    for (let i = 0; i < 1; i++) {
+        particleArray.push(new Particle(x, y));
         console.log(particleArray.length)
     }
-    
-       
+
+
 }
 
-function handleParticles(){
-    for(let i = 0; i < particleArray.length; i++){
-        if(particleArray[i].x > canvas.x || particleArray[i].y > canvas.y || particleArray[i].x < 0 || particleArray[i].y < 0){
-            particleArray.splice(i,1);
-            i --;
-        }else{
-        particleArray[i].update();
+function handleParticles() {
+    for (let i = 0; i < particleArray.length; i++) {
+        let s = (particleArray[i].size / 2)
+        if (particleArray[i].x > canvas.x + s || particleArray[i].y > canvas.y + s ||
+            particleArray[i].x < 0 - s || particleArray[i].y < 0 - s) {
+            
+            particleArray.splice(i, 1);
+            i--;
+        } else {
+            particleArray[i].update();
 
             particleArray[i].draw();
         }
@@ -86,10 +89,10 @@ function handleParticles(){
 }
 
 
-function animate(){
+function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     handleParticles();
-    requestAnimationFrame(animate); 
+    requestAnimationFrame(animate);
 }
 
 
